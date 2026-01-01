6e2b3f2353d3df7b8ee92891df3b763b30015a35
Majidd <123636929+Dmajid24@users.noreply.github.com>
2026-01-01 19:11:29 +0700

    Update Final3

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ use App\Http\Controllers\CategoryController;
 
 
 Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return redirect(Auth::user()->redirectTo());
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
